c4f58b83216ca82e9cf52cce2e754f701ae6bb67
wonhee <asdkcone13@gmail.com>
2022-11-22 19:21:43 +0900

    first commit

---
+++ And03_LinearLayout/app/src/main/java/com/example/and03_linearlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.and03_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.util.Log.d("김원희", "안드로이드 실행 로그");
+    }
+}
